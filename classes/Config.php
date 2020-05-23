a102058191c8a74d4ea6019a800c8cad56783a99
UkletiHolandez <tech.damir@gmail.com>
2020-05-23 09:44:49 +0200

    Update Config.php

--- classes/Config.php
+++ classes/Config.php
@@ -6,7 +6,7 @@
         const DB_NAME = 'prodavnica';
         const DB_SOURCE = 'mysql:host=' . self::DB_HOST . ';dbname=' . self::DB_NAME . ';charset=utf8';
         const DB_USER = 'root';
-        const DB_PASSWORD = 'Admin123';
+        const DB_PASSWORD = 'xxxxx';
 
     }
 
